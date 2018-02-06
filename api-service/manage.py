bf94099c21d19a55bc8f9ba23bffe5fcf0ebff4d
sasiliyu <sasil.adetunji@gmail.com>
2018-02-06 20:03:24 +0100

    postgress setup

--- api-service/manage.py
+++ api-service/manage.py
@@ -1,9 +1,16 @@
 from flask_script import Manager
 
-from api import app
+from api import app, db
 
 
 manager = Manager(app)
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == '__main__':
