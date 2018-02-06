ea91abe98b05553ec595add2de682d780810ad23
sasiliyu <sasil.adetunji@gmail.com>
2018-02-06 19:02:15 +0100

    adds docker config dockerfile and docker-compose file

--- api-service/api/__init__.py
+++ api-service/api/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 
 
@@ -5,7 +6,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('api.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
 
 # sanity check
 @app.route('/users/ping', methods=['GET'])
